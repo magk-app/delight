9934742276c9b061530d474f7b378b525bb4bde3
Jack Luo <30420522+thejackluo@users.noreply.github.com>
2025-11-09 21:43:05 -0500

    1.1: task 2 frontend config part 2

--- packages/frontend/tailwind.config.ts
+++ packages/frontend/tailwind.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
     container: {
@@ -74,7 +74,6 @@ const config: Config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
 
-export default config
-
+export default config;
